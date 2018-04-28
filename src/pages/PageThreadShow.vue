aeb0379ad9178ad1862a0bffe833593fcfb5f3c1
Tony Fatouros <tony.fatouros@gmail.com>
2018-04-29 01:06:56 +1000

    Component Tidy Up

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,38 +1,17 @@
 <template>
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
-
-    <div class="post-list">
-      <div v-for="(postId,idx) in thread.posts" :key="idx" class="post">
-
-        <div class="user-info">
-            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-
-            <a href="#">
-                <img class="avatar-large" :src="users[posts[postId].userId].avatar">
-            </a>
-
-            <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-            <div>
-              {{posts[postId].text}}
-            </div>
-        </div>
-
-        <div class="post-date text-faded">
-            {{posts[postId].publishedAt}}
-        </div>
-      </div>
-    </div>
+    <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
-
+  import PostList from '@/components/PostList'
   export default {
+    components: {
+      PostList
+    },
     props: {
       id: {
         required: true,
@@ -41,9 +20,14 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id],
-        posts: sourceData.posts,
-        users: sourceData.users
+        thread: sourceData.threads[this.id]
+      }
+    },
+    computed: {
+      posts () {
+        const postIds = Object.values(this.thread.posts)
+        return Object.values(sourceData.posts)
+          .filter(post => postIds.includes(post['.key']))
       }
     }
   }
